72c47c78d3d0028679676fff9aa6b135dd3f31d6
martintomov <101264514+martintomov@users.noreply.github.com>
2024-09-20 16:04:56 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
+# __init__.py
 
+from .clip_text_encode_split import (
+    RawText,
+    RawTextCLIPEncode,
+    StringEmptyCheck,
+    RawTextCombine,
+    RawTextReplace
+)
+
+NODE_CLASS_MAPPINGS = {
+    "RawText": RawText,
+    "RawTextEncode": RawTextCLIPEncode,
+    "StringEmptyCheck": StringEmptyCheck,
+    "RawTextCombine": RawTextCombine,
+    "RawTextReplace": RawTextReplace
+}
